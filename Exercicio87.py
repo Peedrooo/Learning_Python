bd589e4f71752152dec3c800abbc2dfa65edd939
Peedrooo <pedrovitora.jesus@gmail.com>
2021-06-03 22:29:03 -0300

    Exercício matriz completo

--- Exercicio87.py
+++ Exercicio87.py
@@ -1,13 +1,31 @@
 matriz = [[],[],[]]
+par = terc = 0
+segun = []
+for c in range(3):
+    for d in range(3):
+        valor = (int(input(f'Valor para {[c, d]} ')))
+        matriz[c].append(valor)
+        if valor %2 == 0:
+            par += valor
+        if d == 2:
+            terc += valor
+        if c == 1:
+            segun.append(valor)
 
-for d in range(0,3):
-    for c in range(0,3):
-        matriz[d].append(int(input(f"Digite um valor para {[d,c]} ")))
-print("-="*20)
+print("-="*15)
 
-for linha in range(len(matriz)):
-    for coluna in range(len(matriz)):
-        print(f"[{matriz[coluna][linha]:^5}]",end='')
-    print("\n")
+for c in range(3):
+    for d in range(3):
+        print(f"[{matriz[c][d]:^3}]", end=' ')
+    print()
+print("-="*15)
+
+print(f'''A soma dos valores pares é = {par}
+A soma dos valores da terceira coluna é = {terc}
+O maior valor da segunda linha é = {max(segun)}
+''')
+
+
+
 
 
