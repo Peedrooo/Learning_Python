2f816c9a5c14fcd07ebf1eef180fa084431171bd
Peedrooo <pedrovitora.jesus@gmail.com>
2021-06-11 18:55:27 -0300

    Trabalhando no exercício 95

--- Exercício95.py
+++ Exercício95.py
@@ -2,9 +2,11 @@ def linha():
     print('-='*20)
     return()
 c = "s"
+cont = 0
 jogo = {}
 jogadores = []
-while c not in "Nn": 
+while c not in "Nn":
+    jogo["Cod"] = cont 
     jogo["Nome"] = input("Nome do jogador ")
     jogos = int(input(f'Quantas partidas {jogo["Nome"]} jogou? '))
     jogo["Gols"] = []
@@ -14,15 +16,32 @@ while c not in "Nn":
     jogadores.append(jogo.copy())
     jogo.clear()
     c = input('Gostaria de continuar ? ').upper()
+    cont += 1
 
 linha()
 print('cod  nome         gols        total')
 linha()
 
-for e,v in enumerate (jogadores):
-    print(e, v.values())
+for v in enumerate (jogadores):
+    print(v["Cod"], v["Nome"], v["Gols"],v["Total"])
 linha()
 
+esc = int(input("Mostrar dados de qual jogador ? "))
+
+
+while esc != 999:
+    if esc not in range(len(jogadores)) :
+        print(f'ERRO! Não exite jogador com o código {esc}! Tente novamente')
+    else:
+        for c in range(0,len(jogadores)):
+            if 
+            
+        
+
+
+                
+    esc = int(input("Mostrar dados de qual jogador ? "))
+print('FINALIZANDO')
 
 
 
--- testes.py
+++ testes.py
@@ -1,2 +1,7 @@
-for c in range(1,5):
-    print(c)
+cl_cadastrados = [{'nome': 'ana', 'cpf': '1000', 'endereco': 'rua xxxx'}, {'nome': 'carlos', 'cpf': '7770', 'endereco': 'Rua aaaa'}]
+nome = (input("Digite o nome que deseja buscar: "))   
+for i in range(0,len(cl_cadastrados)):
+    cl = cl_cadastrados[i]
+
+    if (nome == cl['nome']):
+        print(cl["cpf"])
