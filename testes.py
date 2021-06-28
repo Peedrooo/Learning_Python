39a2ef647b880c54a7577e8bc4cf3adfac4d2062
Peedrooo <pedrovitora.jesus@gmail.com>
2021-06-27 22:30:49 -0300

    Comentário teste

--- testes.py
+++ testes.py
@@ -1,4 +1,4 @@
-''' 
+
 cl_cadastrados = [{'nome': 'ana', 'cpf': '1000', 'endereco': 'rua xxxx'}, {'nome': 'carlos', 'cpf': '7770', 'endereco': 'Rua aaaa'}]
 nome = (input("Digite o nome que deseja buscar: "))   
 for i in range(0,len(cl_cadastrados)):
@@ -45,4 +45,3 @@ while True:
     
         
 print("PROGRAMA FINALIZADO")     
-'''
