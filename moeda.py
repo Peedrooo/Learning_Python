f80e589e33ad7942270b7a8e5ac1325bd7f66da4
Peedrooo <pedrovitora.jesus@gmail.com>
2021-07-08 18:25:07 -0300

    Desenvolvendo exercício 107

---
+++ moeda.py
@@ -0,0 +1,15 @@
+def aumentar(valor):
+    total = valor + valor * 10/100
+    return total
+
+def diminuir(valor):
+    total = valor - valor * 13/100
+    return total
+
+def dobro(valor):
+    total = valor * 2
+    return total
+
+def metade(valor):       
+    total = valor / 2       
+    return total
