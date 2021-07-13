87e74e2a8bcc744cd4df4b68897e81a93df11d92
Peedrooo <pedrovitora.jesus@gmail.com>
2021-07-13 19:29:23 -0300

    Realizando correções propostas

--- utilidadesCeV/moeda/moeda.py
+++ utilidadesCeV/moeda/moeda.py
@@ -1,34 +1,34 @@
 def aumentar(valor,Conversão = True):
     total = valor + valor * 10/100
     if Conversão:       
-        return 'R$ {:.2f}'.format(total)
+        return ('R$ {:.2f}'.format(total))
     else:
         return (total)
 
 def diminuir(valor,Conversão = True):
     total = valor - valor * 13/100
     if Conversão:       
-        return 'R$ {:.2f}'.format(total)
+        return ('R$ {:.2f}'.format(total))
     else:
         return (total)
 
 def dobro(valor,Conversão = True):
     total = valor * 2
     if Conversão:       
-        return 'R$ {:.2f}'.format(total)
+        return ('R$ {:.2f}'.format(total))
     else:
         return (total)
 
 def metade(valor,Conversão = True):       
     total = valor / 2
     if Conversão:       
-        return 'R$ {:.2f}'.format(total)
+        return ('R$ {:.2f}'.format(total))
     else:
         return (total)
 
 def moeda(valor,Conversão = True):
     if Conversão:
-        return 'R$ {:.2f}'.format(valor)
+        return ('R$ {:.2f}'.format(valor))
     else:
         return (valor)
 def linha():
