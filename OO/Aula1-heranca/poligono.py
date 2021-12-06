224dd2e9c4d67de0ee5213c5c99c7d90708896ed
Peedrooo <pedrovitora.jesus@gmail.com>
2021-12-06 20:20:53 -0300

    Relocação de arquivos

---
+++ OO/Aula1-heranca/poligono.py
@@ -0,0 +1,58 @@
+# Criando classe Poligono
+class Poligono:  
+
+    # Construtor                                               
+    def __init__(self, numero_de_lados):
+        self.n = numero_de_lados
+        self.lados = [0 for i in range(numero_de_lados)]
+
+    # metodo 
+    def le_lados(self):
+        self.lados = [float(input("Digite tamanho do lado " + str(i+1) + ": "))
+                      for i in range(self.n)]
+
+    def mostra_lados(self):
+        for i in range(self.n):
+            print("Lado", i+1, "tem comprimento", self.lados[i])
+
+# Criando classe Retangulo, herdando de Poligono
+class Triangulo(Poligono):
+
+    # Alterando o construtor
+    def __init__(self):
+        Poligono.__init__(self, 3)
+
+    def area(self):
+        a, b, c = self.lados
+        # calcula o semi-perímetro
+        s = (a + b + c) / 2
+        area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+        print(f'A área do triângulo é {area:0.2f}')
+
+
+# Criando classe Retangulo, herdando de Poligono
+class Retangulo(Poligono):
+
+    def __init__(self):
+        Poligono.__init__(self, 4)
+
+    def le_lados(self):
+        lado1 = float(input("Digite tamanho do lado 1: "))
+        lado2 = float(input("Digite tamanho do lado 2: "))
+        self.lados[0] = self.lados[2] = lado1
+        self.lados[1] = self.lados[3] = lado2
+
+    def area(self):
+        return self.lados[0] * self.lados[1]
+
+    def diagonal(self):
+        return (self.lados[0] ** 2 + self.lados[1] ** 2) ** 0.5
+
+
+# Criando classe TrianguloRetangulo, herdando de Poligono
+class TrianguloRetangulo(Triangulo):
+    def éTrianguloRetangulo(self):
+        return(self.lados[0]**2 == self.lados[1]**2 + self.lados[2]**2
+               or self.lados[1]**2 == self.lados[0]**2 + self.lados[2]**2
+               or self.lados[2]**2 == self.lados[0]**2 + self.lados[1]**2)
+
