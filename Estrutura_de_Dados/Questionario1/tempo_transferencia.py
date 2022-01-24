0b9386a5eaead94a148bf6d26b4666b966046c7e
Peedrooo <pedrovitora.jesus@gmail.com>
2022-01-23 21:46:22 -0300

    Refatoracao exercicio tempo_tranferencia

--- Estrutura_de_Dados/Questionario1/tempo_transferencia.py
+++ Estrutura_de_Dados/Questionario1/tempo_transferencia.py
@@ -2,23 +2,24 @@
 
 from math import ceil
 
-
 total_bytes = int(input().strip())
 print(f'Transmitindo {total_bytes} bytes...')
 
 tempo = 0
+transferencia = []
+
 while True:
     tempo +=1
-    transferencia = int(input())
-    total_bytes -= transferencia
-    if total_bytes <= 0:
+    transferencia.append(int(input().strip()))
+    resta = total_bytes - sum(transferencia)
+    if resta == 0:
         break
     if tempo % 5 == 0:
-        if transferencia == 0:
+        try:
+            media_transferencia = ceil(round(resta/(sum(transferencia[::-1][0:5])/5),3))
+            print(f'Tempo restante: {media_transferencia} segundos.')
+        except ZeroDivisionError:
             print('Tempo restante: pendente...')
-        else:
-            print(f'Tempo restante: {ceil(total_bytes/transferencia)} segundos.')
 print(f'Tempo total: {tempo} segundos.')
 
 
- 
