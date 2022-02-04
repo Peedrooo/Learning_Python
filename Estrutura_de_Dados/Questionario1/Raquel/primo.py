4f3808d7167d9fa8a05827b20e08b790b8279184
Peedrooo <pedrovitora.jesus@gmail.com>
2022-02-03 22:46:29 -0300

    Correção

--- Estrutura_de_Dados/Questionario1/Raquel/primo.py
+++ Estrutura_de_Dados/Questionario1/Raquel/primo.py
@@ -1,22 +1,24 @@
-def e_primo(entrada):
-    if entrada == 1: return False
+def e_primos(entrada):
+    if entrada == 1:return False
     else:
-        for v in range(2,entrada):
+        for v in range(2, entrada):
             if entrada % v == 0:
                 return False
         return True
 
-
-def gemeos(entrada):
-    gemos_primos = []
-    aux1,aux2,cont = 0,1,3
+def primos_gemeos(entrada):
+    gemeos = []
+    cont, p1, p2 = 0, 1, 3
 
     while cont < entrada:
         while True:
-            aux1,aux2 = aux1+2,aux2+2
-            if e_primo(aux1) and e_primo(aux2):
-                if aux1 + 2 == aux2:
-                    gemos_primos.append((aux1,aux2))
+            p1, p2 = p1 + 2, p2 + 2
+            if e_primos(p1) and e_primos(p2):
+                if p1 + 2 == p2:
+                    gemeos.append(tuple((p1, p2)))
                     break
         cont += 1
-    return gemos_primos
+    return gemeos
+
+
+print(primos_gemeos(2))
