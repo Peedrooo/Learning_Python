3485fa1746ae205426f6a66ef5e2c9d010958235
Peedrooo <pedrovitora.jesus@gmail.com>
2022-03-16 13:07:35 -0300

    Aula e ex de estrutura de dados

---
+++ Estrutura_de_Dados/Questionario4/sabacchard.py
@@ -0,0 +1,15 @@
+qnt = 6
+cartas = ('7 8 3 4 5 10').split()
+combinacao = []	
+
+
+""" 
+def maior_valor(carta,qnt):
+    qnt -= 1  
+    if qnt == 0:
+        break
+    else:
+        
+
+
+print(maior_valor(cartas,qnt)) """
